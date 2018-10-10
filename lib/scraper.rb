45012501176129434da2feb6f1afe4dd7adfdb1a
Gina Fedder <ginafedder@yahoo.com>
2018-10-10 16:50:42 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,17 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+    doc.css("div.student-card").map do |student|
+      student_hash = {
+        name: ,
+        location: ,
+        profile_url: student.css("a").attr
+      }
   end
 
   def self.scrape_profile_page(profile_url)
